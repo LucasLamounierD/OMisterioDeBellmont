1a35b861e25054c065d6adee04ff0a877e1c95fc
Lucas Lamounier <lucaslamounier13@gmail.com>
2019-09-01 13:50:05 -0300

    First Commit
    Inserção dos primeiros arquivos do projeto.

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = 2.5f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftShift) && speed < 5)
+            speed += 0.1f;
+        else if(!Input.GetKey(KeyCode.LeftShift) && speed > 2.5)
+            speed -= 0.1f;
+
+
+        if (Input.GetKey(KeyCode.W))
+            transform.Translate(0, 0, speed * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.A))
+            transform.Translate(-speed * Time.deltaTime, 0, 0);
+
+        if (Input.GetKey(KeyCode.D))
+            transform.Translate(speed * Time.deltaTime, 0, 0);
+
+        if (Input.GetKey(KeyCode.S))
+            transform.Translate(0, 0, -speed * Time.deltaTime);
+
+    }
+}
