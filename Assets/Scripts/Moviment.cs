52feb48446c73d8ba5a40ff05c9f851323bfbfc2
Lucas Lamounier <lucaslamounier13@gmail.com>
2019-09-06 15:45:24 -0300

    First Person Camera

---
+++ Assets/Scripts/Moviment.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Moviment : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Animator animation;
+
+    void Start()
+    {
+        animation = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKey(KeyCode.W))
+            animation.SetBool("Front", true);
+        else
+            animation.SetBool("Front", false);
+
+        if (Input.GetKey(KeyCode.S))
+            animation.SetBool("Back", true);
+        else
+            animation.SetBool("Back", false);
+
+        if (Input.GetKey(KeyCode.A))
+            animation.SetBool("Left", true);
+        else
+            animation.SetBool("Left", false);
+
+        if (Input.GetKey(KeyCode.D))
+            animation.SetBool("Right", true);
+        else
+            animation.SetBool("Right", false);
+    }
+}
