3edfb59079cd3296789905003f11ab290e3f77d8
kevin74Unifei <kevin.vieira@yahoo.com.br>
2019-09-13 17:02:14 -0300

    aoba

---
+++ Assets/Scripts/FootSteps.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+public class FootSteps : MonoBehaviour
+{
+    //[SerializeField]
+    //private AudioClip[] stoneClips;
+    //[SerializeField]
+    //private AudioClip[] mudClips;
+    //[SerializeField]
+    //private AudioClip[] grassClips;
+    [SerializeField]
+    private AudioClip[] clips;
+
+    private AudioSource audioSource;
+    //private TerrainDetector terrainDetector;
+
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+        //terrainDetector = new TerrainDetector();
+    }
+
+    private void Step()
+    {
+        AudioClip clip = GetRandomClip();
+        audioSource.PlayOneShot(clip);
+    }
+
+    private AudioClip GetRandomClip()
+    {
+        return clips[UnityEngine.Random.Range(0, clips.Length)];
+        /*int terrainTextureIndex = terrainDetector.GetActiveTerrainTextureIdx(transform.position);
+
+        switch(terrainTextureIndex)
+        {
+            case 0:
+                return stoneClips[UnityEngine.Random.Range(0, stoneClips.Length)];
+            case 1:
+                return mudClips[UnityEngine.Random.Range(0, mudClips.Length)];
+            case 2:
+            default:
+                return grassClips[UnityEngine.Random.Range(0, grassClips.Length)];
+        }*/
+
+    }
+}
